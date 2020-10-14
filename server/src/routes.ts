e5111e8e2f1fe1194e7e9b9af32eac14e327414b
Romario Da Silva <roma.dasilva94@gmail.com>
2020-10-14 19:48:26 -0300

    First route

---
+++ server/src/routes.ts
@@ -0,0 +1,37 @@
+import { Router, Request, Response } from "express";
+import { getRepository } from "typeorm";
+import Orphanage from "./models/Orphanage";
+
+const routes = Router();
+
+routes.post("/orphanages", async (request: Request, response: Response) => {
+  console.log("Criando um orfanato");
+
+  const {
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends,
+  } = request.body;
+
+  const orphanagesRepository = getRepository(Orphanage);
+
+  const orphanage = orphanagesRepository.create({
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends,
+  });
+
+  await orphanagesRepository.save(orphanage);
+
+  return response.status(201).json(orphanage);
+});
+
+export default routes;
